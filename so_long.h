1f42d92547e042c9313cff94cd58990d4d33a74e
Tetiana Fedorenko <93830487+tfedorenko@users.noreply.github.com>
2022-05-05 17:57:17 +0200

    Update so_long.h

---
+++ so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 18:24:54 by tfedoren          #+#    #+#             */
+/*   Updated: 2022/05/04 19:05:30 by tfedoren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+
+# include "minilbx/mlx.h"
+# include "Libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include <unistd.h>
+# include  <stdio.h>
+# include <stdlib.h>
+
+#endif
